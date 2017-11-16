3aee1ff7f3413332e7d9a3692837321b1a8a14c2
David J. Thomas <dave.a.base@gmail.com>
2017-11-16 12:01:41 -0500

    user roles updated

--- server/controllers/roles.js
+++ server/controllers/roles.js
@@ -4,25 +4,26 @@ const bcrypt = require('bcrypt');
 
 const models = require('../models');
 
-var Role = models.Role;
+var Model = models.Role;
 
 
 const list = (req, res) => {
   // return all roles and their orders
-  return Role.findAll({
+  return Model.findAll({
+    'order': [['order', 'ASC']],
     'attributes': [
       'role',
       'order'
     ]
   })
-      .then(roles => res.status(200).send(roles))
-      .catch(error => res.status(400).send(error));
+    .then(roles => res.status(200).send(roles))
+    .catch(error => res.status(400).send(error));
 };
 
 const create = (req, res) => {
-  return Role.create({
+  return Model.create({
       'role': req.body.role,
-      'order': Role.findAll().length + 1
+      'order': Model.findAll().length + 1
     })
     .then(role => res.status(200).send(role))
     .catch(error => res.status(400).send(error));
@@ -30,19 +31,67 @@ const create = (req, res) => {
 
 const update = (req, res) => {
   var attributes = {};
+  var updateRecord = null;
   if (req.body.role) {
     attributes.role = req.body.role;
   }
-  return Role
-    .update({
+  else {
+    return false;
+  }
+  updateRecord = Model.findById(attributes.role)
+    .then((updateRecord) => {
+      return updateRecord
+        .update(attributes)
+        .then(role => res.status(200).send(role))
+        .catch(error => res.status(400).send(error));
+    });
+};
 
-    })
-    .then(role => res.status(200).send(role))
-    .catch(error => res.status(400).send(error));
+const increment = (req, res) => {
+  if (!req.body.role) {
+    return false;
+  }
+  Model.findById(!req.body.role)
+    .then((currentRecord) => {
+      Model.findAll({where: {order: currentRecord.order + 1}})
+        .then((otherRecord) => {
+          if(!otherRecord) {
+            return false;
+          }
+          currentRecord.order += 1;
+          otherRecord.order -= 1;
+          currentRecord.save();
+          otherRecord.save();
+          return currentRecord;
+        });
+    });
+};
+
+const decrement = (req, res) => {
+  if (!req.body.role) {
+    return false;
+  }
+  Model.findById(!req.body.role)
+    .then((currentRecord) => {
+      if (currentRecord.order === 1) {
+        return false;
+      }
+      Model.findAll({where: {order: currentRecord.order - 1}})
+        .then((otherRecord) => {
+          if(!otherRecord) {
+            return false;
+          }
+          currentRecord.order -= 1;
+          otherRecord.order += 1;
+          currentRecord.save();
+          otherRecord.save();
+          return currentRecord;
+        });
+    });
 };
 
 const destroy = (req, res) => {
-  Role.findAll({'role': req.params.role})
+  Model.findAll({'role': req.params.role})
     .then(role => {
       return role.destroy()
         .then(() => res.status(204).send())
@@ -54,5 +103,7 @@ module.exports = {
   'list': list,
   'create': create,
   'update': update,
+  'increment': increment,
+  'decrement': decrement,
   'destroy': destroy
 };
--- server/routes/api/v1/roles.js
+++ server/routes/api/v1/roles.js
@@ -9,6 +9,8 @@ const controllers = require('../../../controllers');
 router.get('/', controllers.roles.list);
 router.post('/', passport.authenticate('jwt', { session: false }), controllers.roles.create);
 router.put('/:role', passport.authenticate('jwt', { session: false }), controllers.roles.update);
+router.put('/:role/increment', passport.authenticate('jwt', { session: false }), controllers.roles.increment);
+router.put('/:role/decrement', passport.authenticate('jwt', { session: false }), controllers.roles.decrement);
 router.delete('/role', passport.authenticate('jwt', { session: false }), controllers.roles.destroy);
 
 module.exports = router;
