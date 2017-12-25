5101087c9f347306b760d14ba4b4f125fb84f488
David J. Thomas <dave.a.base@gmail.com>
2017-12-25 14:01:30 -0500

    static assets fix and slight static reorg

--- server/utils/staticAssets.js
+++ server/utils/staticAssets.js
@@ -21,26 +21,26 @@ module.exports = {
     // if in a testing or production env, only return minified css
     if (!utils.env.isDevEnv()) {
       return [
-        path.join(__dirname, assets.dirs.static, assets.dirs.dist, 'lib.min.css'),
-        path.join(__dirname, assets.dirs.static, assets.dirs.dist, 'app.min.css')
+        path.join(assets.dirs.dist, 'lib.min.css'),
+        path.join(assets.dirs.dist, 'app.min.css')
       ];
     }
     // build and append vendor css filepaths
     let vendorPaths = utils.paths.buildPaths(
       utils.paths.basenamesFromPaths(assets.vendor.css),
-      path.join(__dirname, assets.dirs.static, assets.dirs.lib, assets.dirs.css)
+      path.join(assets.dirs.lib, assets.dirs.css)
       // remove folder info since after static files are collected dir structure is lost
     );
     // build and append internal css filepaths
     let internalCSSPaths = utils.paths.buildPaths(
       assets.source.css,
-      path.join(__dirname, assets.dirs.static, assets.dirs.style, assets.dirs.css)
+      path.join(assets.dirs.styles, assets.dirs.css)
 
     );
     // build and append internal less filepaths
     let internalLessPaths = utils.paths.buildPaths(
       assets.source.less,
-      path.join(__dirname, assets.dirs.static, assets.dirs.style, assets.dirs.less)
+      path.join(assets.dirs.styles, assets.dirs.less)
     );
     // merge items of each list into single array and return
     return [...vendorPaths, ...internalCSSPaths, ...internalLessPaths];
@@ -55,20 +55,20 @@ module.exports = {
     // if in a testing or production env, only return minified js
     if (!utils.env.isDevEnv()) {
       return [
-        path.join(__dirname, assets.dirs.static, assets.dirs.dist, 'lib.min.js'),
-        path.join(__dirname, assets.dirs.static, assets.dirs.dist, 'app.min.js'),
+        path.join(assets.dirs.dist, 'lib.min.js'),
+        path.join(assets.dirs.dist, 'app.min.js'),
       ];
     }
     // build and append vendor js filepaths
     let vendorPaths = utils.paths.buildPaths(
       // remove folder info since after static files are collected dir structure is lost
       utils.paths.basenamesFromPaths(assets.vendor.js),
-      path.join(__dirname, assets.dirs.static, assets.dirs.lib, assets.dirs.js)
+      path.join(assets.dirs.lib, assets.dirs.scripts)
     );
     // build and append internal js filepaths
     let internalPaths = utils.paths.buildPaths(
       assets.source.js,
-      path.join(__dirname, assets.dirs.static, assets.dirs.js)
+      path.join(assets.dirs.scripts)
     );
     // merge items of each list into single array and return
     return [...vendorPaths, ...internalPaths];
